eb8a1625c5101ee274a2c0b508854b35d575c283
getsh91 <getsheshetu19@gmail.com>
2024-01-23 15:19:28 -0800

    setup for firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:t_store/app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:t_store/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+      .then((FirebaseApp value) => Get.put(AuthenticationRepository()));
   runApp(const App());
 }
